e09875548807e9f106d08cfb7c2bf314f600e3a0
Joe Maller <joe@joemaller.com>
2023-11-12 15:41:49 -0500

    List directories and get info for a known file

--- README.md
+++ README.md
@@ -8,13 +8,14 @@ Ultimately, all I want to do is append a row to this Excel file, on OneDrive: [*
 ## Goals:
 
 - [x] Connect to Azure using the [Graph API Console app](https://learn.microsoft.com/en-us/entra/identity-platform/tutorial-v2-nodejs-console) 
-- [ ] Call other API endpoints (not just [getUsers](https://learn.microsoft.com/en-us/graph/api/user-list?view=graph-rest-1.0&tabs=http))
+- [x] Call other API endpoints (not just [getUsers](https://learn.microsoft.com/en-us/graph/api/user-list?view=graph-rest-1.0&tabs=http))
 - [x] Get Users' Drives (OneDrive)
-- [ ] List contents of Users' Drives
-- [ ] List directory/contents of a Drive/[DriveItem](https://learn.microsoft.com/en-us/graph/api/resources/driveitem?view=graph-rest-1.0)
-- [ ] List the file or file attributes for the [target Excel sheet](https://ideasonpurpose-my.sharepoint.com/:x:/r/personal/iop_ideasonpurpose_com/Documents/Active%20Jobs/IOP/IOP021_%20BNB/Brand%20New%20Brand!%20Cycle%205/a_Mangement/Contact%20Form%20Test%20Sheet.xlsx?d=web3ac602a73a4e21835edfb4cbd490a8&csf=1&web=1&e=iuD8jT)
+- [x] List contents of Users' Drives
+- [x] List directory/contents of a Drive/[DriveItem](https://learn.microsoft.com/en-us/graph/api/resources/driveitem?view=graph-rest-1.0)
+- [x] List the file or file attributes for the [target Excel sheet](https://ideasonpurpose-my.sharepoint.com/:x:/r/personal/iop_ideasonpurpose_com/Documents/Active%20Jobs/IOP/IOP021_%20BNB/Brand%20New%20Brand!%20Cycle%205/a_Mangement/Contact%20Form%20Test%20Sheet.xlsx?d=web3ac602a73a4e21835edfb4cbd490a8&csf=1&web=1&e=iuD8jT)
+- [ ] Append a row to an excel sheet
 
 Additional stuff: 
 - [x] Replace Axios with native Fetch API
-- [ ] Actual API error messages 
+- [x] Actual API error messages 
 - [ ] Convert to ES Modules
--- bin/index.js
+++ bin/index.js
@@ -12,7 +12,7 @@ const options = yargs.usage("Usage: --op <operation_name>").option("op", {
   alias: "operation",
   describe: "operation name",
   type: "string",
-  choices: ["getUsers", "getUser", "getDrive", "getDriveRoot"],
+  choices: ["getUsers", "getUser", "getDrive", "getDriveChildren", "getSheet"],
   demandOption: true,
 }).argv;
 
@@ -20,7 +20,7 @@ const apiBase = process.env.GRAPH_ENDPOINT + "/v1.0";
 async function main() {
   console.log(`You have selected: ${options.op}`, options);
 
-  let result, error;
+  let result, driveId;
 
   // here we get an access token
   const authResponse = await auth.getToken(auth.tokenRequest);
@@ -54,12 +54,36 @@ async function main() {
         );
         break;
 
-      case "getDriveRoot":
-        const driveId =
+      case "getDriveChildren":
+        driveId =
           "b!tOvj5m3TxUuAQeyG1BWmJyPUbJ5L1slOqVB4aPdgGD2qfEMY5ue0R6EVDM71kgjB";
 
+        // const itemPath = 'Active Jobs';
+        const itemPath =
+          "Active Jobs/IOP/IOP021_ BNB/Brand New Brand! Cycle 5/a_Mangement";
+        // https://ideasonpurpose-my.sharepoint.com/personal/iop_ideasonpurpose_com/Documents/Active%20Jobs
+        // https://ideasonpurpose-my.sharepoint.com/personal/iop_ideasonpurpose_com/Documents/Active%20Jobs/IOP/IOP021_%20BNB/Brand%20New%20Brand!%20Cycle%205/a_Mangement/Contact%20Form%20Test%20Sheet.xlsx
+        // NOTE: (big note) Check the webUrl to discover the effective root directory
+        //       Requesting Users' OneDrive directories puts us in /Documents, eg.
+        //           /personal/iop_ideasonpurpose_com/Documents
+        //       This means any reqeusts for children should reference known folders
+        //       No idea why the plain /root: doesn't' work
         result = await fetch.callApi(
-          `${apiBase}/drives/${driveId}/root:/`,
+          `${apiBase}/drives/${driveId}/root:/${itemPath}:/children`,
+          // `${apiBase}/drives/${driveId}/root:/Active Jobs/IOP/IOP021_ BNB/Brand New Brand! Cycle 5/a_Mangement/Contact Form Test Sheet.xlsx`,
+          accessToken
+        );
+        break;
+
+      case "getSheet":
+        driveId =
+          "b!tOvj5m3TxUuAQeyG1BWmJyPUbJ5L1slOqVB4aPdgGD2qfEMY5ue0R6EVDM71kgjB";
+
+        const filePath =
+          "Active Jobs/IOP/IOP021_ BNB/Brand New Brand! Cycle 5/a_Mangement";
+        const fileName = "Contact Form Test Sheet.xlsx";
+        result = await fetch.callApi(
+          `${apiBase}/drives/${driveId}/root:/${filePath}/${fileName}`,
           accessToken
         );
         break;
