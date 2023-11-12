04d1245f0f24c73be7b0a803a37406345b9fde22
Joe Maller <joe@joemaller.com>
2023-11-12 11:46:39 -0500

    refactor, more README

--- bin/index.js
+++ bin/index.js
@@ -1,42 +1,77 @@
 #!/usr/bin/env node
 
 // read in env settings
-require('dotenv').config();
+require("dotenv").config();
 
-const yargs = require('yargs');
+const yargs = require("yargs");
 
-const fetch = require('./fetch');
-const auth = require('./auth');
+const fetch = require("./fetch");
+const auth = require("./auth");
 
-const options = yargs
-    .usage('Usage: --op <operation_name>')
-    .option('op', { alias: 'operation', describe: 'operation name', type: 'string', demandOption: true })
-    .argv;
+const options = yargs.usage("Usage: --op <operation_name>").option("op", {
+  alias: "operation",
+  describe: "operation name",
+  type: "string",
+  choices: ["getUsers", "getUser", "getDrive", "getDriveRoot"],
+  demandOption: true,
+}).argv;
 
+const apiBase = process.env.GRAPH_ENDPOINT + "/v1.0";
 async function main() {
-    console.log(`You have selected: ${options.op}`);
+  console.log(`You have selected: ${options.op}`, options);
 
-    switch (yargs.argv['op']) {
-        case 'getUsers':
+  let result, error;
 
-            try {
-                // here we get an access token
-                const authResponse = await auth.getToken(auth.tokenRequest);
+  // here we get an access token
+  const authResponse = await auth.getToken(auth.tokenRequest);
+  const { accessToken } = authResponse;
 
-                // call the web API with the access token
-                const users = await fetch.callApi(auth.apiConfig.uri, authResponse.accessToken);
+  try {
+    switch (yargs.argv["op"]) {
+      case "getUsers":
+        const users = await fetch.callApi(`${apiBase}/users`, accessToken);
+        result = users.value
+          .filter((u) => !u.userPrincipalName.includes("#EXT#"))
+          .map(({ displayName, userPrincipalName }) => ({
+            displayName,
+            userPrincipalName,
+          }));
+        break;
 
-                // display result
-                console.log(users);
-            } catch (error) {
-                console.log(error);
-            }
+      case "getUser":
+        result = await fetch.callApi(
+          `${apiBase}/users/${options._[0]}`,
+          accessToken
+        );
+        break;
 
-            break;
-        default:
-            console.log('Select a Graph operation first');
-            break;
+      case "getDrive":
+        const driveUser = options._[0] ?? "iop@ideasonpurpose.com";
+        result = await fetch.callApi(
+          `${apiBase}/users/${driveUser}/drive`,
+          accessToken
+        );
+        break;
+
+      case "getDriveRoot":
+        const driveId =
+          "b!tOvj5m3TxUuAQeyG1BWmJyPUbJ5L1slOqVB4aPdgGD2qfEMY5ue0R6EVDM71kgjB";
+
+        result = await fetch.callApi(
+          `${apiBase}/drives/${driveId}/root:/`,
+          accessToken
+        );
+        break;
+
+      default:
+        result = "Select a Graph operation first";
+        break;
     }
-};
+
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 main();
